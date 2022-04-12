8dfc4d300f4720b7822ae5836e1df5d8f6461525
Oliver <oliver20669@gmail.com>
2022-04-12 23:25:49 +0200

    Setup

---
+++ script.js
@@ -0,0 +1,41 @@
+let startButton = document.querySelector(".start");
+
+startButton.addEventListener("click", main);
+
+function main(){
+
+
+
+}
+
+let Game = (function (){
+
+
+
+    return{
+
+    };
+})();
+
+let Board = (function (){
+
+
+
+    return{
+
+    };
+})();
+
+let Player = () => {
+
+
+
+    return {};
+}
+
+let Square = () => {
+
+    
+
+    return {};
+}
